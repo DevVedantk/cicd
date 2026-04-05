e2f2b338bc6d4d036036fe815e25c950e6021089
vedant-khasbage <vedantkhasbage2005@gmail.com>
2026-04-05 11:23:32 +0530

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+const PORT = 4000;
+
+// middleware
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
+
+// sample API
+app.get("/api/data", (req, res) => {
+  res.json({
+    message: "Hello from backend",
+    success: true
+  });
+});
+
+// POST example
+app.post("/api/user", (req, res) => {
+  const { name } = req.body;
+
+  res.json({
+    message: `User ${name} created`,
+  });
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
